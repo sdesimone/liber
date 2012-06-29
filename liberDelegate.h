394edad5a6e16d79b9b4f0a884d3b476afe5f57c
Sergio De Simone <sdesimone@gmx.net>
2012-06-29 11:56:43 +0200

    added delegate

---
+++ liberDelegate.h
@@ -0,0 +1,16 @@
+//
+//  liberDelegate.h
+//  liberdoc
+//
+//  Created by Sergio De Simone on 2/15/10.
+//  Copyright 2010 HP Spain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "Sparkle.framework/Headers/SUUpdater.h"
+
+@interface LiberDelegate : SUUpdater {
+
+}
+
+@end
---
+++ liberDelegate.m
@@ -0,0 +1,42 @@
+//
+//  liberDelegate.m
+//  liberdoc
+//
+//  Created by Sergio De Simone on 2/15/10.
+//  Copyright 2010 HP Spain. All rights reserved.
+//
+
+#import "liberDelegate.h"
+
+@implementation LiberDelegate
+
+- (id)init
+{
+	return [super init];
+}
+
+- (void)applicationWillFinishLaunching:(NSNotification *)aNotification
+{
+	[self setAutomaticallyChecksForUpdates:YES];
+	[self setAutomaticallyDownloadsUpdates:YES];
+	[self checkForUpdatesInBackground];
+//	[self checkForUpdates:self];
+}
+
+- (void)updater:(SUUpdater *)updater didFindValidUpdate:(SUAppcastItem *)update
+{
+	NSLog(@"Found Update!!!!");
+}
+
+// Sent immediately before installing the specified update.
+- (void)updater:(SUUpdater *)updater willInstallUpdate:(SUAppcastItem *)update
+{
+	NSLog(@"Trying to install!!!!");
+}
+
+- (void)updaterDidNotFindUpdate:(SUUpdater *)update
+{
+	NSLog(@"Did not find any update!!!!");
+}
+
+@end
