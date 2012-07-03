28a8b9779829b1ca09fd91d0759359f0cfa93f93
Sergio De Simone <sdesimone@gmx.net>
2012-07-03 11:15:29 +0200

    document model for app

--- liberDocument.m
+++ liberDocument.m
@@ -14,6 +14,12 @@
 
 @implementation LiberDocument
 
+/**********************************************************
+ ******************
+ ****************** INITIALIZATION AND COCOA INTEGRATION
+ ******************
+ **********************************************************/
+
 - (id)init 
 {
     self = [super init];
@@ -35,6 +41,26 @@
     return @"liberDocument";
 }
 
+/**********************************************************
+ ******************
+ ****************** ACTIONS
+ ******************
+ **********************************************************/
+
+- (IBAction)addNewLiber:(id)sender
+{
+	[_arrayController add:sender];
+	[self performSelector: @selector(delayedScroll)
+             withObject: nil
+             afterDelay: 0.1];
+}
+
+/**********************************************************
+ ******************
+ ****************** HELPER METHODS
+ ******************
+ **********************************************************/
+
 -(NSFont*)currentFont
 {
 	NSFontManager *fontManager = [NSFontManager sharedFontManager];
@@ -45,14 +71,6 @@
 {
 	[_tableView scrollRowToVisible:[_tableView numberOfRows]-1];
 	[_drawerWindow makeFirstResponder:_firstTextField];
-}
-
-- (IBAction)addNewLiber:(id)sender
-{
-	[_arrayController add:sender];
-	[self performSelector: @selector(delayedScroll)
-             withObject: nil
-             afterDelay: 0.1];
 }
 
 - (NSNumber*)maxLiberID
@@ -77,6 +95,12 @@
 	return lastImageID;
 }
 
+/**********************************************************
+ ******************
+ ****************** PRINTING SUPPORT
+ ******************
+ **********************************************************/
+
 - (NSPrintInfo*)printInfo {
     NSPrintInfo *pInfo = [super printInfo];
     if (!_setUpPrintInfoDefaults) {
@@ -91,7 +115,7 @@
     }
     return pInfo;
 }
-
+ 
 - (void)updatePrintView
 {
 	NSPrintInfo* pInfo = [self printInfo];
@@ -103,6 +127,9 @@
 	[_printView noteHeightOfRowsWithIndexesChanged:[NSIndexSet indexSetWithIndexesInRange:r]];
 }
 
+//-- following method is deprecated.
+//-- printDocumentWithSettings:showPrintPanel:delegate:...
+//-- should be used instead.
 - (void)printShowingPrintPanel:(BOOL)showPanels
 {
 	NSPrintInfo* pInfo = [self printInfo];
@@ -123,26 +150,13 @@
 				didRunSelector:NULL
 				 contextInfo:NULL];
 }
-/*
-- (NSNumber *)currentLiberID
-{
-	return _currentLiberID;
-}
 
-- (NSNumber *)newCurrentLiberID
-{
-	[self setCurrentLiberID:[NSNumber numberWithInt:[_currentLiberID intValue] + 1]];
-	return _currentLiberID;
-}
-
-- (void)setCurrentLiberID:(NSNumber *)curLibID
-{
-    if (curLibID != _currentLiberID) {
-        [_currentLiberID release];
-        _currentLiberID = [curLibID retain];
-    }
-}
-
+/**********************************************************
+ ******************
+ ****************** METADATA MANAGEMENT
+ ******************
+ **********************************************************/
+ 
 - (BOOL)setMetadataForStoreAtURL:(NSURL *)url
 {
     NSPersistentStoreCoordinator *psc = [[self managedObjectContext] persistentStoreCoordinator];
@@ -152,8 +166,8 @@
         if (metadata == nil) {
             metadata = [NSMutableDictionary dictionary];
         }
-				//-- custom logics
-				[metadata setObject:[self currentLiberID] forKey:@"NextUnusedLiberID"];
+				//-- custom logics to update metadata
+//				[metadata setObject:[self currentLiberID] forKey:@"NextUnusedLiberID"];
         [psc setMetadata:metadata forPersistentStore:pStore];
         return YES;
     }
@@ -167,38 +181,81 @@
     if (pStore != nil) {
         NSMutableDictionary *metadata = [[[psc metadataForPersistentStore:pStore] mutableCopy] autorelease];
         if (metadata == nil) {
-            [self setCurrentLiberID:[self maxLiberID]];;
+//-- set default value for metadata
+//            [self setCurrentLiberID:[self maxLiberID]];;
         } else {
-            [self setCurrentLiberID:[metadata objectForKey:@"NextUnusedLiberID"]];
+//-- read stored value for metadata
+//            [self setCurrentLiberID:[metadata objectForKey:@"NextUnusedLiberID"]];
 				}
         return YES;
     }
     return NO;
 }
 
-//-- update metadata on open/create
-- (BOOL)configurePersistentStoreCoordinatorForURL:(NSURL *)url
-    ofType:(NSString *)fileType
-    modelConfiguration:(NSString *)configuration
-    storeOptions:(NSDictionary *)storeOptions
-    error:(NSError **)error
-{
-    BOOL ok = [super configurePersistentStoreCoordinatorForURL:url
-                ofType:fileType modelConfiguration:configuration
-                storeOptions:storeOptions error:error];
+/**********************************************************
+ ******************
+ ****************** OPEN/SAVE OVERRIDES
+ ******************
+ **********************************************************/
 
-    if (ok) {
-        NSPersistentStoreCoordinator *psc = [[self managedObjectContext]
-                persistentStoreCoordinator];
-        NSPersistentStore *pStore = [psc persistentStoreForURL:url];
-        id existingMetadata = [[psc metadataForPersistentStore:pStore]
-                objectForKey:(NSString *)kMDItemKeywords];
-        if (existingMetadata == nil) {
-            ok = [self setMetadataForStoreAtURL:url];
-        }
-    }
-    return ok;
+//-- update metadata on open/create
+- (BOOL)configurePersistentStoreCoordinatorForURL:(NSURL*)url
+    ofType:(NSString*)fileType
+    modelConfiguration:(NSString*)configuration
+    storeOptions:(NSDictionary*)storeOptions
+    error:(NSError**)error
+{
+	NSMutableDictionary *options = nil;
+	if (storeOptions != nil)
+		options = [storeOptions mutableCopy];
+	else
+		options = [[NSMutableDictionary alloc] init];
+	
+	NSPersistentStoreCoordinator* psc = [[self managedObjectContext] persistentStoreCoordinator];
+	NSDictionary* sourceMetadata = [NSPersistentStoreCoordinator metadataForPersistentStoreOfType:nil URL:url error:error];
+	if (sourceMetadata == nil)
+			NSLog(@"No metadata found.");
+	
+	BOOL pscCompatibile = [[psc managedObjectModel]
+							isConfiguration:nil
+							compatibleWithStoreMetadata:sourceMetadata];
+
+	if (!pscCompatibile) {
+		NSLog(@"Will migrate.");
+
+		NSFileManager* fileManager = [NSFileManager defaultManager];			
+		NSString* dst = @"~/Library/Application Support/liber/";
+		dst = [dst stringByExpandingTildeInPath];
+		if ([fileManager fileExistsAtPath:dst] == NO)
+			[fileManager createDirectoryAtPath:dst attributes:nil];
+		dst = [dst stringByAppendingPathComponent:@"backups/"];
+		if ([fileManager fileExistsAtPath:dst] == NO)
+			[fileManager createDirectoryAtPath:dst attributes:nil];
+		NSDate* bckDate = [[NSDate alloc] init];
+		NSString* bckStr = [bckDate descriptionWithCalendarFormat:@"%y%m%d-%H%M%S" timeZone:nil locale:nil];
+		[bckDate release];
+		dst = [dst stringByAppendingPathComponent:[bckStr stringByAppendingFormat:@"-%@.%@", [[url path] lastPathComponent], @"zip"]];
+
+		NSTask* bckTask = [[NSTask alloc] init];
+		[bckTask setLaunchPath:@"/usr/bin/ditto"];
+		[bckTask setArguments:
+			[NSArray arrayWithObjects:@"-c", @"-k", @"--rsrc", [url path], dst, nil]];		
+		[bckTask launch];
+		[bckTask waitUntilExit];
+		if ([bckTask terminationStatus] != 0)
+			NSLog(@"Sorry, didn't work.");
+		[bckTask release];
+		
+		[options setObject:[NSNumber numberWithBool:YES] forKey:NSMigratePersistentStoresAutomaticallyOption];
+	} else {
+		NSLog(@"Migration not necessary.");
+	}
+
+	BOOL ok = [super configurePersistentStoreCoordinatorForURL:url ofType:fileType modelConfiguration:configuration storeOptions:options error:error];
+	[options release], options = nil;
+	return ok;
 }
+
 //-- update metadata on save
 - (BOOL)writeToURL:(NSURL *)absoluteURL
     ofType:(NSString *)typeName
@@ -221,10 +278,10 @@
 		NSEnumerator* enumerator = [[*error userInfo] keyEnumerator];
 		id e = nil;
 			while ((e = [enumerator nextObject])) {
-			NSLog(@"error: %@\n", [[*error userInfo] valueForKey:e]);
+			NSLog(@"error: (%d/%@) %@\n", [*error code], [*error domain], [[*error userInfo] valueForKey:e]);
 		}
 	}
 	return b;
 }
-*/
+
 @end
